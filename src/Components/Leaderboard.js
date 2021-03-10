f2ffaabddea2638c492c9cbdf1023f54964ef1ce
AB <ABTikshuv@gmail.com>
2021-03-10 16:43:16 +0200

    credentials

--- src/Components/Leaderboard.js
+++ src/Components/Leaderboard.js
@@ -1,26 +1,27 @@
 
 import React, { useState, useEffect } from 'react';
-import LeaderboardChart  from './LeaderboardChart';
+import LeaderboardChart from './LeaderboardChart';
 import LeaderboardTable from './LeaderboardTable';
-import {BASEURL} from '../config.js';
+import { BASEURL } from '../config.js';
+import Axios from 'axios';
 
 function Leaderboard() {
 
     const [leaderboard, setLeaderboard] = useState(null);
-    
+
     useEffect(() => {
         fetchLeaderboard();
     }, [])
 
     function fetchLeaderboard() {
-        fetch(BASEURL + '/leaderboard',{withCredentials: true}).then(res => res.json()).then(json => {
+        Axios.get(BASEURL + '/leaderboard', { withCredentials: true }).then(res => res.json()).then(json => {
             setLeaderboard(json);
-        }).catch((e)=>{});
+        }).catch((e) => { });
     }
 
 
     return (
-        <div style={{ display: "flex"}}>
+        <div style={{ display: "flex" }}>
             <LeaderboardChart leaderboard={leaderboard} />
             <LeaderboardTable leaderboard={leaderboard} />
         </div>
